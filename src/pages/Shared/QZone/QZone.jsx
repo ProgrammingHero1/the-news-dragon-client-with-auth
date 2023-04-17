88e6f36df2221ad351145bc9aec2c6fc44fa3b0f
Programming Hero <programming.hero1@gmail.com>
2023-04-16 23:38:13 -0500

    added right side nav options

---
+++ src/pages/Shared/QZone/QZone.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import qzone1 from '../../../assets/qZone1.png'
+import qzone2 from '../../../assets/qZone2.png'
+import qzone3 from '../../../assets/qZone3.png'
+
+const QZone = () => {
+    return (
+        <div className='bg-secondary text-center my-4 py-4'>
+            <h4>Q-Zone</h4>
+            <div>
+                <img src={qzone1} alt="" />
+                <img src={qzone2} alt="" />
+                <img src={qzone3} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default QZone;
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav</h2>
+            <h4>Login With</h4>
+            <Button className='mb-2' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"> <FaGithub></FaGithub> Login with Github</Button>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item> <FaFacebook></FaFacebook> facebook</ListGroup.Item>
+                    <ListGroup.Item> <FaTwitter></FaTwitter> twitter </ListGroup.Item>
+                    <ListGroup.Item><FaInstagram></FaInstagram> instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
