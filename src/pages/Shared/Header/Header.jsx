fc9093a6b5983522180eb096eb3e8747e5e56f21
Programming Hero <programming.hero1@gmail.com>
2023-04-18 10:49:47 -0500

    created login and register route

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,15 +1,11 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../providers/AuthProvider';
-import { FaUserCircle } from 'react-icons/fa';
+
 
 const Header = () => {
-
-    const { user } = useContext(AuthContext);
 
     return (
         <Container className='mt-4'>
@@ -24,34 +20,7 @@ const Header = () => {
                     I can be a React component, multiple React components, or just some text...... I can be a React component, multiple React components, or just some text....
                 </Marquee>
             </div>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Nav.Link href="#features">
-                                <Link to="/">Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
-                            <Nav.Link href="#pricing">Career</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            {user && <Nav.Link href="#deets">
-                                <FaUserCircle style={{ fontSize: '2rem' }}></FaUserCircle>
-                            </Nav.Link>
-                            }
-                            <Nav.Link eventKey={2} href="#memes">
-                                {user ?
-                                    <Button variant="secondary">Logout</Button> :
-                                    <Link to="/login">
-                                        <Button variant="secondary">Login</Button>
-                                    </Link>
-                                }
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            
         </Container>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,22 +1,38 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
 import NewsLayout from "../layouts/NewsLayout";
 import News from "../pages/News/News/News";
+import LoginLayout from "../layouts/LoginLayout";
+import Login from "../pages/Login/Login/Login";
+import Register from "../pages/Login/Register/Register";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Main></Main>,
+        element: <LoginLayout></LoginLayout>,
         children: [
             {
                 path: '/',
-                element: <Category></Category>,
-                loader: () => fetch('http://localhost:5000/news')
+                element: <Navigate to="/category/0"></Navigate>
             },
             {
-                path: '/category/:id',
+                path: 'login',
+                element: <Login></Login>
+            },
+            {
+                path: 'register',
+                element: <Register></Register>
+            }
+        ]
+    },
+    {
+        path: 'category',
+        element: <Main></Main>,
+        children: [
+            {
+                path: ':id',
                 element: <Category></Category>,
                 loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
             }
